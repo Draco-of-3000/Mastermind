477c1ce959aed0922fcbf3800ba43d48bca9552f
Draco <kevin.tenisson@gmail.com>
2023-05-11 04:52:58 -0700

    Created Mastermind class and method skeletons

---
+++ mastermind.rb
@@ -0,0 +1,58 @@
+class Mastermind
+    def color_array
+
+    end
+
+    def cpu_code_maker
+
+    end
+
+    def cpu_code_breaker
+
+    end
+
+    def get_player_names
+
+    end
+
+    def get_number_of_games
+
+    end
+
+    def count_guess
+
+    end
+
+    def wrong_feedback
+
+    end
+
+    def mid_feedback
+
+    end
+
+    def correct_feedback
+
+    end
+
+    def count_points
+
+    end
+
+    def assign_points
+
+    end
+
+    def swap_roles
+
+    end
+
+    def new_game
+
+    end
+
+    def declare_winner
+
+    end
+end
+
