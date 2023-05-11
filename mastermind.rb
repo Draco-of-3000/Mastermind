2adb18c4ab1c024d60cdb77817fd6b817d8aa1a1
Draco <kevin.tenisson@gmail.com>
2023-05-11 09:32:32 -0700

    Defined human_selection method

--- mastermind.rb
+++ mastermind.rb
@@ -1,12 +1,25 @@
 class Mastermind
-    @@Colors = ["Red", "Green", "Blue", "Yellow", "Orange", "Purple", "Brown","Pink"]
+    @@colors = ["Red", "Green", "Blue", "Yellow", "Orange", "Purple", "Brown","Pink"]
     @@cpu_code = " "
+    @@human_code = []
+    @@cpu_points = 0
+
     def cpu_selection
         @random_colors = @@Colors.sample(4).map(&:downcase)
         @@cpu_code = @random_colors
         return @@cpu_code
     end
     
+    def human_selection
+        puts "Pick 4 colors out of #{@@colors} in any order you want to be your code"
+        
+        until @@human_code.size == 4
+            @input = gets.chomp.downcase
+            @@human_code.push(@input)
+        end
+        @@human_code
+    end
+
     def cpu_code_maker
         
     end
@@ -61,4 +74,4 @@ class Mastermind
 end
 
 game = Mastermind.new
-puts game.cpu_selection
+puts game.human_selection
