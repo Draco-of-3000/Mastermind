81066c1a26d887b4ac898e1b8bedb96ba4fce004
Draco <kevin.tenisson@gmail.com>
2023-05-24 04:06:10 -0700

    Modified cpu_code_breaker method

--- mastermind.rb
+++ mastermind.rb
@@ -146,6 +146,16 @@ class Mastermind
 
             puts code_maker_score
 
+            @cpu_code.each_with_index do |color, index|
+                if @@human_code[index] == color
+                  puts correct_feedback
+                elsif @@human_code.include?(color)
+                  puts mid_feedback
+                else
+                  puts wrong_feedback
+                end
+            end
+
             puts "Code Breaker's guess = #{cpu_selection}"
             count_code_maker_points
             count_guess
